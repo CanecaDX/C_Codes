a5931cbdd42a8db6380e9f2d50f63968c0ac6d20
CanecaDX <manzkeleonardo@gmail.com>
2026-03-26 19:02:48 -0300

    entrega2

--- data_structure/entrega2.c
+++ data_structure/entrega2.c
@@ -1,3 +1,7 @@
+// Lista 2
+// 26/03/26
+// Leonardo Manzke, leonardomanzke.aluno@unipampa.edu.br
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -20,37 +24,71 @@ typedef struct bandas{
 } bandas;
 
 bandas* preencheStruct();
-void exibeStruct(bandas *listaB);
+void exibeStruct(bandas *listaB, int numBandas);
 void exibeUma(bandas listaB);
-void exibeRanking(bandas *listaB, int numRank);
-void exibeGenero(bandas *listaB, int tipo);
+void exibeRanking(bandas *listaB, int numRank, int numBandas);
+void exibeGenero(bandas *listaB, int tipo, int numBandas);
+void nomeFav(bandas *listaB, char nomeBanda[50], int numBandas);
 
 int main (){
 	
-	
 	bandas *listaB = NULL;
-	int numRank = 0, tipo;
+	int numRank = 0, tipo = 0, opcao = 1, numBandas = 0;
+	char nomeBanda[50];
 	
-	listaB = preencheStruct();
-	exibeStruct(listaB);
-	
-	printf("Insira um numero de 1 a 5: ");
-	scanf("%d", &numRank);
-	
-	exibeRanking(listaB, numRank);
-	
-	printf("\nInsira o genero que deseja buscar: ");
-	scanf("%d", &tipo);
+		while(opcao != 0){
+		printf("\n------------------------------------");
+		printf("\n0_ Encerra programa");
+		printf("\n1_ Inserir bandas.");
+		printf("\n2_ Mostrar bandas.");
+		printf("\n3_ Mostrar banda por genero");
+		printf("\n4_ Mostrar bandas por ranking");
+		printf("\n5_ Mostrar bandas pelo nome");
+		printf("\n------------------------------------");
+		printf("\nInforme a ação desejada: ");
+		scanf("%d", &opcao);
+		
+		switch(opcao){
+			case 0: 
+				printf("\nEncerrando programa");
+				free(listaB);
+			break;
+			case 1:
+				printf("Quantas bandas deseja inserir? ");
+				scanf("%d", &numBandas);
+				listaB = preencheStruct(listaB, numBandas);
+			break;
+			case 2:
+				exibeStruct(listaB, numBandas);
+			break;
+			case 3:
+				printf("\n");
+				printf("\nInsira o genero que deseja buscar (1 - rock, 2 - metal, 3 - mpb, 4 - pagode, 5 - sertanejo, 6 - nu metal, 7 - eletronica): ");
+				scanf("%d", &tipo);
+				exibeGenero(listaB, tipo, numBandas);
+			break;
+			case 4:
+				printf("\nInsira a posicao do ranking (1 a 5): ");
+				scanf("%d", &numRank);
+				exibeRanking(listaB, numRank, numBandas);
+			break;
+			case 5:
+				printf("Insira o nome da banda que deseja buscar: ");
+				scanf("%s", nomeBanda);
+				nomeFav(listaB, nomeBanda, numBandas);
+			break;
+		}
+	}
 
  return 0;			
 }
 
-bandas* preencheStruct(){
+bandas *preencheStruct(bandas *p, int numBandas){
 	
 	int i = 0;
-	bandas *p = (bandas*)malloc(3 * sizeof(bandas));
+	p = (bandas*)malloc(numBandas * sizeof(bandas));
 	
-	for(i = 0; i<3; i++){
+	for(i = 0; i<numBandas; i++){
 	 printf("\nInsira dados da %d° banda: ", i+1);
 	 printf("\nInsira o nome da banda: ");
 	 scanf("%s", p[i].nome);
@@ -72,11 +110,11 @@ bandas* preencheStruct(){
  return p;
 }
 
-void exibeStruct(bandas *listaB){
+void exibeStruct(bandas *listaB, int numBandas){
 	
 	int i = 0;
 	
-	for(i = 0; i<4; i++){
+	for(i = 0; i < numBandas; i++){
 		exibeUma(listaB[i]);
 	}
 }
@@ -88,49 +126,66 @@ void exibeUma(bandas listaB){
 	 printf("\nGenero da banda: ");
 	 switch(listaB.tipo){
 				case 1:
-					printf("Metal\n");
+					printf("Rock");
 					break;
 				case 2:
-					printf("Rock\n");
+					printf("Metal");
 					break;
 				case 3:
-					printf("MPB\n");
+					printf("MPB");
 					break;
 				case 4:
-					printf("Pagode\n");
+					printf("Pagode");
 					break;
 				case 5:
-					printf("Sertanejo\n");
+					printf("Sertanejo");
 					break;
 				case 6:
-					printf("Nu Metal\n");
+					printf("Nu Metal");
 					break;
 				case 7:
-					printf("Eletronica\n");
+					printf("Eletronica");
 					break;
 				}
 	 printf("\nNúmero de integrantes da banda: %d", listaB.integrantes);
 	 printf("\nRanking da banda (top 5): %d", listaB.ranking);
 }
 
-void exibeRanking(bandas *listaB, int numRank){
+void exibeRanking(bandas *listaB, int numRank, int numBandas){
 	
 	int i = 0;
 	
-	for(i = 0; i<4; i++){
+	for(i = 0; i<numBandas; i++){
 		 if(listaB[i].ranking == numRank){
 			 exibeUma(listaB[i]);
 		 }
 	}
 }
 
-void exibeGenero(bandas *listaB, int tipo){
+void exibeGenero(bandas *listaB, int tipo, int numBandas){
 	
 	int i = 0;
 	
-	for(i = 0; i<4; i++){
+	printf("\nBandas com este genero na lista:");
+	for(i = 0; i<numBandas; i++){
 		 if(listaB[i].tipo == tipo){
 			 exibeUma(listaB[i]);
 		 }
 	}
+}
+
+void nomeFav(bandas *listaB, char nomeBanda[50], int numBandas){
+	
+	int i;
+
+	for(i = 0; i<numBandas; i++){
+		if(strcmp(nomeBanda, listaB[i].nome) == 0 && listaB[i].ranking < 5){
+			printf("\nEssa banda esta no seu top 5!");
+			exibeUma(listaB[i]);
+		}
+		else{
+			printf("\nEssa banda nao esta no seu top 5!");
+		}
+	}
+	
 }
